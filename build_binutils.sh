c132fcc9f83207d506e2b8633b14b861ec6159e0
sheep <2799661540@qq.com>
2025-10-30 03:21:11 +0800

    add proxy option

--- build_binutils.sh
+++ build_binutils.sh
@@ -7,24 +7,32 @@
 BINUTILS_VERSION=""
 BUILD_TARGET=""
 INSTALL_PATH=""
+PROXY_HOST=""
 SCRIPT_NAME=$(basename "$0")
+
+Okay="\033[32m[+]\033[0m"
+ERR="\033[31m[-]\033[0m"
 
 usage() {
     cat << EOF
-usage: $SCRIPT_NAME [options]
+    usage: $SCRIPT_NAME [options]
 
-Required options:
-    -v. -- version VERSION Set installation version
-    -t. -- build_target TARGET setting to build the target architecture
-Optional options:
-    -h. -- help Display this help information
-Example:
-    $SCRIPT_NAME -i /usr/local -v 2.38 -t aarch64-linux-gnu
-    $SCRIPT_NAME --install_path /usr/local --version 2.38 --build_target aarch64-linux-gnu
+    Required options:
+        -i. --install_path install path
+        -v. --version binutils version
+        -t. --build_target target architecture
+
+    Optional options:
+        -h. --help Display this help information
+
+    Example:
+        $SCRIPT_NAME -i /usr/local -v 2.38 -t aarch64-linux-gnu
+        $SCRIPT_NAME --install_path /usr/local --version 2.38 --build_target aarch64-linux-gnu
+        $SCRIPT_NAME --install_path /usr/local --proxy 192.168.1.123:4567 --version 2.38 --build_target aarch64-linux-gnu
 EOF
 }
 
-TEMP=$(getopt -o i:v:t:h --long install_path:,version:,build_target:,help -n '$SCRIPT_NAME' -- "$@")
+TEMP=$(getopt -o i:v:t:p:h --long install_path:,version:,build_target:,proxy:,help -n '$SCRIPT_NAME' -- "$@")
 
 if [ $? != 0 ]; then
     echo "Error: Parameter parsing failed !"
@@ -46,6 +54,10 @@ while true; do
             ;;
         -t|--build_target)
             BUILD_TARGET="$2"
+            shift 2
+            ;;
+        -p|--proxy)
+            PROXY_HOST="$2"
             shift 2
             ;;
         -h|--help)
@@ -70,47 +82,49 @@ if [[ -z "$INSTALL_PATH" || -z "$BINUTILS_VERSION" || -z "$BUILD_TARGET" ]]; the
 fi
 
 function download_binutils() {
-    
-    local flag=0
+    local proxy="-e use_proxy=yes -e https_proxy=https://$PROXY_HOST"
     mkdir -p source
-    
-    echo "Download binutils source code: binutils-$BINUTILS_VERSION.tar.xz"
-    if ! wget -q --show-progress -O "./source/binutils-$BINUTILS_VERSION.tar.xz" "https://sourceware.org/pub/binutils/releases/binutils-$BINUTILS_VERSION.tar.xz"; then
-        echo "Error: Download failed !"
-        flag=1
+    if [[ ! -f source/binutils-$BINUTILS_VERSION.tar.xz ]]; then
+        echo "Download binutils source code: binutils-$BINUTILS_VERSION.tar.xz"
+        if ! wget $proxy -q --show-progress -O "./source/binutils-$BINUTILS_VERSION.tar.xz" "https://sourceware.org/pub/binutils/releases/binutils-$BINUTILS_VERSION.tar.xz"; then
+            echo -e "$ERR Error: Download failed !"
+        fi
+        echo -e "$Okay Download Successful !"
     fi
-
-    return $flag
 }
 
 function build_binutils() {
-    
-    local flag=0
-
     echo "  Install_Path: $INSTALL_PATH"
     echo "  Version: binutils-$BINUTILS_VERSION"
     echo "  BuildTarget: $BUILD_TARGET"
+    if [[ ! -d binutils-$BINUTILS_VERSION ]]; then
+        echo "Decompressing source code"
+        if ! tar -xvf source/binutils-$BINUTILS_VERSION.tar.xz 2>&1 > tar-binutils-$BINUTILS_VERSION.log; then
+            echo -e "$ERR Error: Decompressing failed !"
+        fi
+        echo -e "$Okay Decompressing Successful !"
+    fi
+    (
+        mkdir -p binutils-$BINUTILS_VERSION/{build,logs} && cd binutils-$BINUTILS_VERSION/build
+        
+        echo "Compile environment detection"
+        if ! ../configure --prefix=$INSTALL_PATH/$BINUTILS_VERSION --disable-gdb --disable-gdbserver --disable-weeror --disable-nls --target=$BUILD_TARGET > ../logs/configure.log 2>&1; then
+            echo -e "$ERR Environmental anomaly !"
+        fi
+        echo -e "$Okay Compile environment Okay !"
+        echo "Compiling"
+        if ! (make -j$(nproc --all) > ../logs/build.log 2>&1 && make install > ../logs/install.log 2>&1); then
+            echo -e "$ERR Error: Compile failed !"
+        fi
+        echo -e "$Okay Compile Successful !"
+    )
+}
 
-    echo "Decompressing source code"
-    if ! tar -xvf ./source/binutils-$BINUTILS_VERSION.tar.xz 2>&1 > tar-binutils-$BINUTILS_VERSION.log; then
-        echo "Error: Decompressing failed !"
-        flag=1
-    fi
-    mkdir -p binutils-$BINUTILS_VERSION/build && cd binutils-$BINUTILS_VERSION/build
-    
-    echo "Compile environment detection"
-    if ! ../configure --prefix=$INSTALL_PATH --disable-gdb --disable-gdbserver --disable-weeror --disable-nls --target=$BUILD_TARGET 2>&1 > configure.log; then
-        echo "Environmental anomaly !"
-        flag=1
-    fi
-    echo "Compiling"
-    if ! (make -j$(nproc --all) 2>&1 > build.log && make install 2>&1 > install.log); then
-        echo "Error: Compile failed !"
-        flag=1
-    fi
-
-    return $flag
+function clean_up() {
+    echo -e "$Okay Clean up !"
+    rm -rf binutils-$BINUTILS_VERSION/build/*
 }
 
 download_binutils
 build_binutils
+clean_up
